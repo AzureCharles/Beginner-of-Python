b8c1673da35fd70c94b17ac853d92c3f8eb99e2f
AzureCharles <71918203+AzureCharles@users.noreply.github.com>
2020-10-31 15:36:57 +0800

    Add files via upload

---
+++ name_rd.py
@@ -0,0 +1,13 @@
+#name in random
+
+#reference url:https://blog.csdn.net/qq_41426326/article/details/91975774?utm_medium=distribute.pc_aggpage_search_result.none-task-blog-2~all~first_rank_v2~rank_v28-3-91975774.nonecase&utm_term=%E9%9A%8F%E6%9C%BA%E7%94%9F%E6%88%90%E5%90%8D%E5%AD%97&spm=1000.2123.3001.4430
+
+import pypinyin as ppy
+import random as rd
+
+def name_random:
+    f_name_restore = ""
+    #考虑到常用姓氏有限，以单个字符串储存即可，可视为常数内置
+    m_name_restore = ""
+    #middle name应选取平声或仄声，考虑到与last name有高度重合，应使用共用数据源
+    l_name = 
